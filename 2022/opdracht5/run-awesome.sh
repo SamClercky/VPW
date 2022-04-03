bead70dbe3ddab8916b47a20d359c643071b9084
SamClercky <sam.clercky@gmail.com>
2022-04-03 22:46:55 +0200

    placeholder opdrachten aangemaakt via template

---
+++ 2022/opdracht5/run-awesome.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ -e "__pycache__" ]; then
+    rm -r __pycache__ # make sure we get accurate time measure
+fi
+
+python ./oplossing.py3 <wedstrijd.invoer >wedstrijd.uitvoer_temp
+
+echo "========================== INPUT/EXPECTED/ACTUAL ======================="
+pr -m -t -W 150 wedstrijd.invoer wedstrijd.uitvoer wedstrijd.uitvoer_temp
+echo "=========================== START OUTPUT ==============================="
+delta -sn wedstrijd.uitvoer wedstrijd.uitvoer_temp
+diff_status=$?
+echo "============================ STATISTICS ================================"
+if [ $diff_status -eq 0 ]; then
+    echo "Correct"
+else
+    echo "Not yet correct"
+fi
+
+hyperfine "python ./oplossing.py3 <wedstrijd.invoer"
+rm wedstrijd.uitvoer_temp
+
+echo "=============================== END ===================================="
